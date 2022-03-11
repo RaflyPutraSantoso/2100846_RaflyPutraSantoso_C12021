7be4a730023c1114d305757d2380980e7cca35e7
Rafly Putra Santoso <rafly.putrasantoso380@gmail.com>
2022-03-10 20:14:20 -0800

    commit pertama

---
+++ 2100846.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>This is Rafly Putra Santoso page</h1>
+
+<?php
+echo "Hello World!<br/>";
+echo "My name is Rafly Putra Santoso<br/>";
+echo "I have my own NIM which is 2100846";
+?>
+
+</body>
+</html>
